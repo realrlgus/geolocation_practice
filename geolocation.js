d2eb4f9d42055f71914c06dfb45052fdc3bf3b34
realrlgus <realrlgus@naver.com>
2020-03-07 23:57:06 +0900

    카카오 맵 추가

--- geolocation.js
+++ geolocation.js
@@ -1,14 +1,26 @@
+// 구글 지도를 그린다
+function initialize(latitude, longitude) {
+  var container = document.getElementById("map"); //지도를 담을 영역의 DOM 레퍼런스
+  var options = {
+    //지도를 생성할 때 필요한 기본 옵션
+    center: new kakao.maps.LatLng(latitude, longitude), //지도의 중심좌표.
+    level: 3 //지도의 레벨(확대, 축소 정도)
+  };
+
+  var map = new kakao.maps.Map(container, options); //지도 생성 및 객체 리턴
+}
+
 if ("geolocation" in navigator) {
   var latitude;
   var longitude;
   var geoSuccess = function(position) {
     geoPosition = position;
+
+    // 위도 경도
     latitude = position.coords.latitude;
     longitude = position.coords.longitude;
 
-    // 위도 경도
-
-    console.log(latitude, longitude);
+    initialize(latitude, longitude);
   };
   var geoError = function(error) {
     //   MIS 로그인 불가하도록 처리
@@ -17,7 +29,6 @@ if ("geolocation" in navigator) {
     //   1: permission denied
     //   2: position unavailable (error response from location provider)
     //   3: timed out
-    console.log(error);
   };
   // 사용자의 현재 위치를 구한다.
   navigator.geolocation.getCurrentPosition(geoSuccess, geoError);
